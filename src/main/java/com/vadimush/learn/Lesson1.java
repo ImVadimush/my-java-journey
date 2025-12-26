a52e76e3f7b1b7d29610a3d66bfe5ec95d50bbd5
Vadim Kolbiniev <kolbinievvadim@gmail.com>
2025-12-26 14:05:31 +0100

    First lesson: print Hello World

--- src/main/java/com/vadimush/learn/Lesson1.java
+++ src/main/java/com/vadimush/learn/Lesson1.java
@@ -4,7 +4,7 @@ public class Lesson1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!\n by VadimuIII");
+        System.out.println("Hello World!\n \nby VadimuIII");
 
     }
 }
